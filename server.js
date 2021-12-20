8f8f87bcbf8f5980a89e310f1074935bf12ad475
nwodzis2 <60149019+nwodzis2@users.noreply.github.com>
2021-12-20 01:59:04 -0500

    heroku/server update

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(cors());
 //app.use(require("./routes/record"));
 
 
-const dbo = require("./db/conn");
 // Accessing the path module
 const path = require("path");
 
@@ -20,11 +19,8 @@ app.use(express.static(path.resolve(__dirname, "./wodzisz-portfolio/build")));
 
 // We send whichever file
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "./wodzisz-portfolio/build", "index.html"));
 });
 app.listen(port, () => {
-    dbo.connectToServer(function(err){
-        if(err) console.error(err);
-    });
     console.log(`Server is running on port: ${port}`);
 }); 
